d6df1331fb37dc27f0572bd0926e402d2c266be8
Marionvincenzi <marion.vincenzi7@orange.fr>
2024-08-01 16:46:00 +0200

    js

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,31 +1,83 @@
- async function getWorks() {
-        const works = await fetch("http://localhost:5678/api/works")
-        const worksJson = await works.json()
-        console.log(worksJson)
-        return worksJson
-    }
-    getWorks()    
+const worksUrl = "http://localhost:5678/api/works";
+const urlCategories ="https//localhost:5678/api/categories";
+const gallery = document.querySelector(".gallery");
+const filters = document.querySelector(".filters");
+
+async function getWorks() {
+     await fetch (worksUrl)
+     .then (response => response.json());
+     console.log(response);
+     .then (dataWorks => {
+     const imagesUrl = dataWorks.map (work => work.imagesUrl);
+     const title = dataWorks.map (work => work.title);
+     gallery.innerHTML = "";
+    for (let i = 0; imagesUrl.length; i++); {
+    const figureTag = document.createElement("figure");
+    const imagesTags = document.createElement("img");
+    const figcaptionTitle = document.createElement("figcaption");
+
+    figureTag.classList.add("works");
+    imagesTags.src = imagesUrl[i];
+    figcaptionTitle.innerText = title[i];
+
+    figureTag.appendChild(imagesTags);
+    figureTag.appendChild(figcaptionTitle);
+    gallery.appendChild(figureTag);
+    console.log(response);
+            
 
 
-async function displayWorks() {
-    const arrayWorks = await getWorks
-    arrayWorks.forEach((work) => {
-        const figure = document.createElement("figure")
-        const img = document.createElement("img")
-        const figcaption = document.createElement("figcaption")
-
-        img.src = work.images
-        figcaption.textContent = work.title
-        figure.classList.add("gallery")
-
-        figure.appendChild(img)
-        figure.appendChild(figcaption)
-        gallery.appendChild(figure)
+        }
     })
-    
-}
+    .catch (error => console.error ("erreur récupération des données"));
+    };
+getWorks();
 
-displayWorks()
+
+    async function displayWorks() {
+     const arrayWorks = await getWorks
+     const gallery = document.querySelector(".gallery")
+
+    function createWorks(work) {
+    const figure = document.createElement("figure")
+    const img = document.createElement("img")
+    const figcaption = document.createElement("figcaption")
+
+         img.src = work.images
+        figcaption.textContent = work.title
+         figure.classList.add("gallery-item")
+
+         figure.appendChild(img)
+         figure.appendChild(figcaption)
+         gallery.appendChild(figure)
+     }
+    
+ }
+
+ displayWorks()
+
+ function getCategories(response) {
+     const response = fetch ("https//localhost:5678/api/categories");
+     console.log(response);
+     return response
+ }
+ getCategories()
+
+ const categoriesButton = async () => {
+     const categories = await getCategories()
+ }
+
+ categories.forEach(category => {
+    const btn = document.createElement("button")
+    btn.textContent = category.name.toUpperCase()
+    btn.id = category.id
+    btn.className = "filtersbutton"
+    filters.appendChild(btn)
+    
+ });
+
+
+
 
 
 
